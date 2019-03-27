b69646f0961b85e21cb0a523ad36b96e51243b96
Kyle George <kylepgeorge@gmail.com>
2019-03-27 19:48:45 -0400

    Stage repo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
+import $ from 'jquery';
 import config from '../config.json';
 
 console.log(config);
+
+$(document).on(`click`, `.v-link`, function() {
+  const v = $(this).attr(`data-v`);
+  let href = ``;
+  let newTab = false;
+  if (config[v].mode === `set`) {
+    if (config[v].setContent.type !== `link`) {
+      href = `pages/${v}`;
+    } else {
+      href = config[v].setContent.url;
+      newTab = config[v].setContent.newTab;
+    }
+  } else {
+    const randomItem = config[v].randomContent[Math.floor(Math.random() * config[v].randomContent.length)];
+    if (randomItem.type !== `link`) {
+      href = `pages/${v}`;
+    } else {
+      href = randomItem.url;
+      newTab = randomItem.newTab;
+    }
+  }
+  if (newTab) {
+    window.open(href, `_blank`);
+  } else {
+    window.location.href = href;
+  }
+});
 
 // On click of a v1/v2/v3 link...
 //   If content is set, link to that.
