650027bc278474dc76fa3ffaebdce2b2a6195689
Kyle George <kylepgeorge@gmail.com>
2019-03-27 21:33:54 -0400

    push for testing

--- README.md
+++ README.md
@@ -1,12 +1,85 @@
-# Update: Major Overhaul (9/25/18)
+# HOW TO CONFURE
 
-- Code has been greatly simplified to allow easier editing and updating.
-- Recreated columns and layouts using flexbox.
-- Reorganized folder structure.
-- Moved old content to "backups" folder.
-- Finished migration from Namecheap to GitHub (in progress).
-- Added "shake" animation to homepage buttons.
-
-### TODO
-
-- Add subscriber input box to homepage.
+```json
+{
+	"v1": {
+		"mode": "set",
+		"setContent": {
+			"type": "link",
+			"url": "https://www.youtube.com/watch?v=bybfO8OnORA",
+			"assetUrl": null,
+			"newTab": true
+		},
+		"randomContent": [
+			{
+				"type": "link",
+				"assetUrl": null,
+				"url": "https://www.youtube.com/watch?v=bybfO8OnORA",
+				"newTab": true
+			},
+			{
+				"type": "image",
+				"assetUrl": "/v1/images/image.png",
+				"url": "https://www.youtube.com/watch?v=ro0DCOxxG18",
+				"newTab": true
+			}
+		]
+	},
+	"v2": {
+		"mode": "random",
+		"setContent": {
+			"type": "link",
+			"url": "https://www.youtube.com/watch?v=bybfO8OnORA",
+			"newTab": false
+		},
+		"randomContent": [
+			// {
+			// 	"type": "link",
+			// 	"assetUrl": null,
+			// 	"url": "https://www.youtube.com/watch?v=_CEAarqdAZc",
+			// 	"newTab": true
+			// },
+			// {
+			// 	"type": "link",
+			// 	"assetUrl": null,
+			// 	"url": "https://www.youtube.com/watch?v=XEw4IDzsAhc",
+			// 	"newTab": false
+			// },
+			{
+				"type": "image",
+				"assetUrl": null,
+				"url": null,
+				"newTab": true
+			}
+		]
+	},
+	"v3": {
+		"mode": "random",
+		"setContent": {
+			"type": "link",
+			"url": "https://www.youtube.com/watch?v=bybfO8OnORA",
+			"newTab": false
+		},
+		"randomContent": [
+			{
+				"type": "image",
+				"assetUrl": null,
+				"url": "http://covers.i-d.co/",
+				"newTab": true
+			},
+			{
+				"type": "image",
+				"assetUrl": null,
+				"url": "http://9-eyes.com/",
+				"newTab": true
+			},
+			{
+				"type": "image",
+				"assetUrl": null,
+				"url": "https://www.youtube.com/watch?v=yk7IDVzLiQo",
+				"newTab": true
+			}
+		]
+	}
+}
+```
--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 import $ from 'jquery';
-import config from '../config.json';
+// import config from '../config.json';
 
-console.log(config);
+const contentHtml = {
+  image: assetUrl => `
+    <img src="${assetUrl}" alt="Image"/>
+  `,
+  pdf: assetUrl => `
+  <object width="80%" height="80%" type="application/pdf" data="${assetUrl}">
+    <p>Insert your error message here, if the PDF cannot be displayed.</p>
+  </object>
+  `,
+  video: assetUrl => `
+    <video width="100%" height="100%" controls>
+      <source src="${assetUrl}" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  `,
+  audio: assetUrl => ``,
+};
 
-$(document).on(`click`, `.v-link`, function() {
+$(document).on(`click`, `.v-link`, async function() {
+  const config = await fetch(`https://cdn.jsdelivr.net/gh/kylepg/v1v2v3co_stage@latest/config.json`).then(res => res.json());
   const v = $(this).attr(`data-v`);
   let href = ``;
   let newTab = false;
   if (config[v].mode === `set`) {
     if (config[v].setContent.type !== `link`) {
-      href = `pages/${v}`;
+      href = `/pages/${v}`;
     } else {
       href = config[v].setContent.url;
       newTab = config[v].setContent.newTab;
@@ -17,7 +34,7 @@ $(document).on(`click`, `.v-link`, function() {
   } else {
     const randomItem = config[v].randomContent[Math.floor(Math.random() * config[v].randomContent.length)];
     if (randomItem.type !== `link`) {
-      href = `pages/${v}`;
+      href = `/pages/${v}`;
     } else {
       href = randomItem.url;
       newTab = randomItem.newTab;
@@ -30,13 +47,15 @@ $(document).on(`click`, `.v-link`, function() {
   }
 });
 
-// On click of a v1/v2/v3 link...
-//   If content is set, link to that.
-//
-//   If not, choose a random piece of content and go to that link.
-// If the link is null, go to the v1/v2/v3 page.
-
-// On load of the v1/v2/v3 page...
-// Sort content by once with images (not null)
-// Select one
-// If it has a link (not null), make it clickable.
+$(() => {
+  const config = await fetch(`https://cdn.jsdelivr.net/gh/kylepg/v1v2v3co_stage@latest/config.json`).then(res => res.json());
+  const v = $(`body`).attr(`data-v`);
+  const mediaOptions = config[v].randomContent.filter(item => item.type !== `link`);
+  const randomItem = mediaOptions[Math.floor(Math.random() * config[v].randomContent.length)];
+  let html = contentHtml[randomItem.type](randomItem.assetUrl);
+  // If it has a link (not null), make it clickable. by wrapping in
+  if (randomItem.url !== null) {
+    html = `<a href="${randomItem.url}" ${randomItem.newTab ? `target="_blank"` : ``}>${html}</a><img src="../../media/click-the-image.gif"/>`;
+  }
+  $(`#content`).html(html);
+});
